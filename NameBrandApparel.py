25e2548985ca0bcb30fb5a9e87784adf30bd9ec4
blake-mcguire <blake.lm@outlook.com>
2024-06-19 15:38:52 -0500

    First Commit

---
+++ NameBrandApparel.py
@@ -0,0 +1,564 @@
+
+shopping_cart = {}
+inventory = {
+    't-shirts': {
+        'Calvin Klein': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Tommy Hilfiger': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Polo': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        }
+    },
+    'jeans': {
+        'Levis': {
+            'Black': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Blue': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Grey': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'White': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            }
+        },
+        'Wrangler': {
+            'Black': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Blue': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Grey': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'White': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            }
+        },
+        'American Eagle': {
+            'Black': {
+                '30X30': 10,
+                '32X32': 20,
+                '34x34': 30,
+                '36x36': 40
+            },
+            'Blue': {
+                '30X30': 10,
+                '32x32': 20,
+                '34x34': 30,
+                '36x36': 40
+            },
+            'Grey': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'White': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            }
+        }
+    },
+    'slacks': {
+        'Tom Ford': {
+            'Black': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Grey': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36x36': 40
+            },
+            'White': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Brown': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            }
+        },
+        'Calvin Klein': {
+            'Black': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Grey': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'White': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            },
+            'Brown': {
+                '30X30': 10,
+                '32X32': 20,
+                '34X34': 30,
+                '36X36': 40
+            }
+        }
+    },
+    'shorts': {
+        'Nike': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Adidas': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Under Armour': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        }
+    },
+    'sweatpants': {
+        'Nike': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Adidas': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        },
+        'Under Armour': {
+            'Black': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Grey': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'White': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            },
+            'Red': {
+                'Small': 10,
+                'Medium': 20,
+                'Large': 30,
+                'X-Large': 40
+            }
+        }
+    }
+}
+
+def select_color(inventory, shopping_cart, article_choice, brand_choice):
+    color_choice = input('Pick Your Color: ').lower()
+    if color_choice.title() in inventory[article_choice][brand_choice.title()]:
+        for size in inventory[article_choice][brand_choice.title()][color_choice.title()]:
+            print(f'''                                               
+            **{size}**
+            ''')
+        select_size(inventory, shopping_cart, article_choice, brand_choice, color_choice)
+    else:
+        print('Color not found. Please try again.')
+        select_color(inventory, shopping_cart, article_choice, brand_choice)
+        
+        
+
+def select_size(inventory, shopping_cart, article_choice, brand_choice, color_choice):
+    size_choice = input('Pick Your Size: ').lower()
+    if any(size_choice == size.lower() for size in inventory[article_choice][brand_choice.title()][color_choice.title()]):
+        price = inventory[article_choice][brand_choice.title()][color_choice.title()][size_choice.title()]
+        print(f'''                                               
+            ${price}
+            ''')
+        add_to_cart(inventory, shopping_cart, article_choice, brand_choice, color_choice, size_choice, price)
+    else:
+        print("Size not found. Please try again.")
+        select_size(inventory, shopping_cart, article_choice, brand_choice, color_choice)
+    
+    
+
+def show_clothing(inventory, shopping_cart):
+    lowest_level_browse = input("""
+    Browse...
+    1. T-Shirts
+    2. Jeans
+    3. Slacks
+    4. Shorts
+    5. Sweatpants
+    6. Return 
+    """).lower()
+    
+    if lowest_level_browse.lower() in inventory:
+        for brand in inventory[lowest_level_browse]:
+            print(f'''
+            ====={brand}=====
+            ''')
+        select_brand(inventory, shopping_cart, lowest_level_browse.lower())   
+    else:
+        print('Article not found. Please try again.')      
+        show_clothing(inventory, shopping_cart) 
+   
+
+
+
+def select_brand(inventory, shopping_cart, article_choice):
+    
+    brand_choice = input('Pick Your Poison: ').lower()
+    if any(brand_choice == brand.lower() for brand in inventory[article_choice]):
+        for color in inventory[article_choice][brand_choice.title()]:                                           
+            print(f'''                                               
+            ~~{color}~~
+            ''')
+        select_color(inventory, shopping_cart, article_choice, brand_choice)
+    else:
+        print('Brand not found. Please try again.')
+        select_brand(inventory, shopping_cart, article_choice)
+
+
+
+def add_to_cart(inventory, shopping_cart, article_choice, brand_choice, color_choice, size_choice, price):
+    cart_choice = input('Would you like to add this item to your cart? Yes/No ').lower()
+    if cart_choice == 'yes':
+        shopping_cart[article_choice] = {
+            brand_choice: {
+                color_choice: {
+                    size_choice: price
+                }
+            }
+        }
+        print(f'You have added {size_choice.title()} {color_choice.title()} {brand_choice.title()} {article_choice} to your cart')
+        
+    elif cart_choice == 'no':
+        main(inventory, shopping_cart)
+    else:
+        print('Please enter a valid input')
+        add_to_cart(inventory, shopping_cart, article_choice, brand_choice, color_choice, size_choice, price)
+    
+
+
+
+def show_cart(inventory, shopping_cart):
+    if shopping_cart:
+        for article in shopping_cart:
+            for brand in shopping_cart[article]:
+                for color in shopping_cart[article][brand]:
+                    for size in shopping_cart[article][brand][color]:
+                        print(f'''
+=======================================================
+{color} {brand} {article} - {size} - ${shopping_cart[article][brand][color][size]}''')
+
+        choice = input("""
+            1. Remove Item From Cart
+            2. Checkout
+            3. Return
+                    """)
+        if choice == '1':
+            remove_from_cart(inventory, shopping_cart)
+        elif choice == '2':
+            checkout(inventory, shopping_cart)
+        elif choice == '3':
+            main(inventory, shopping_cart)
+        else:
+            print('Please enter a valid input')
+            show_cart(inventory, shopping_cart)
+    else:
+        print('Your cart is empty')
+        main(inventory, shopping_cart)
+            
+
+
+def remove_from_cart(inventory, shopping_cart):
+    if shopping_cart:
+        article_choice = input('What type of clothing would you like to remove from your cart? ').lower()
+        brand_choice = input('What brand is the article of clothing? ').lower()
+        color_choice = input('What color is the article? ').lower()
+        size_choice = input('What size is it? ').lower()
+        if article_choice in shopping_cart:
+            if brand_choice in shopping_cart[article_choice]:
+                if color_choice in shopping_cart[article_choice][brand_choice]:
+                    if size_choice in shopping_cart[article_choice][brand_choice][color_choice]:
+                        del shopping_cart[article_choice][brand_choice][color_choice][size_choice]
+                        print(f'{size_choice.title()} {color_choice.title()} {brand_choice.title()} {article_choice} has been removed from your cart')
+                        show_cart(inventory, shopping_cart)
+                    else:
+                        print('Size not found. Please try again.')
+                        remove_from_cart(inventory, shopping_cart)
+                else:
+                    print('Color not found. Please try again.')
+                    remove_from_cart(inventory, shopping_cart)
+            else:
+                print('Brand not found. Please try again.')
+                remove_from_cart(inventory, shopping_cart)
+        else:
+            print('Article not found. Please try again.')
+            remove_from_cart(inventory, shopping_cart)
+    else:
+        print('Your cart is empty')
+        main(inventory, shopping_cart)
+
+
+
+def checkout(inventory, shopping_cart):
+    total = 0
+    for article in shopping_cart:
+        for brand in shopping_cart[article]:
+            for color in shopping_cart[article][brand]:
+                for size in shopping_cart[article][brand][color]:
+                    total += shopping_cart[article][brand][color][size]
+    print(f'Your total is: ${total}')
+    print('Thank you for shopping with us!')
+    shopping_cart.clear()
+    main(inventory, shopping_cart)
+
+
+
+def main(inventory, shopping_cart):
+    while True:
+        response = input("""
+          
+     NAME BRAND APPAREL
+  ///////////////////////    
+    1. View Collection
+    2. Show Cart
+    3. Close Tab
+    
+    
+    """)
+        if response == '1' or response.lower() == 'view collection':
+            show_clothing(inventory, shopping_cart)
+        elif response == '2' or response.lower() == 'show cart':
+            show_cart(inventory, shopping_cart)
+        elif response == '3' or response.lower() == 'close tab':
+            break
+        else:
+            print('Please enter a valid input')
+        
+main(inventory, shopping_cart)
